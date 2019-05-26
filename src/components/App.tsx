32017f1aacf56f8ccbdafd00f182a974f6c5e4ad
Paul Kim <paul.kim@catalystrms.com>
2019-05-26 08:18:31 -0600

    Change file type to tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { injectGlobal, ThemeProvider } from 'styled-components';
 
-import { HomePage } from 'components';
+import HomePage from './pages/HomePage';
 
 // https://github.com/diegohaz/arc/wiki/Styling
 import theme from './themes/default';
