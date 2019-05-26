af088ee302b09bd8051a0fadec6390826c789e2f
Paul Kim <paul.kim@catalystrms.com>
2019-05-26 08:19:21 -0600

    Update homepage story

--- src/components/pages/HomePage/index.stories.js
+++ src/components/pages/HomePage/index.stories.js
@@ -1,9 +1,6 @@
 // https://github.com/diegohaz/arc/wiki/Storybook
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { HomePage } from 'components'
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import HomePage from '.';
 
-storiesOf('HomePage', module)
-  .add('default', () => (
-    <HomePage />
-  ))
+storiesOf('HomePage', module).add('default', () => <HomePage />);
