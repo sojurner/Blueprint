05c847e5256283835dbdcf3676f4818b7501d693
Paul Kim <paul.kim@catalystrms.com>
2019-05-26 08:18:14 -0600

    Edit StoryBook config

--- private/storybook/webpack.config.js
+++ private/storybook/webpack.config.js
@@ -1,10 +1,32 @@
-const baseConfig = require('../../webpack.config')
+/* eslint-disable arrow-parens */
+const baseConfig = require('../../webpack.config');
 
-module.exports = storybookBaseConfig => Object.assign({}, storybookBaseConfig, {
-  resolve: Object.assign({}, storybookBaseConfig.resolve, {
-    modules: baseConfig.resolve.modules,
-  }),
-  module: Object.assign({}, storybookBaseConfig.module, {
-    rules: storybookBaseConfig.module.rules.concat(baseConfig.module.rules.slice(1)),
-  }),
-})
+module.exports = storybookBaseConfig => {
+  console.log(baseConfig.module.rules.slice(1));
+  const config = Object.assign({}, storybookBaseConfig, {
+    resolve: Object.assign({}, storybookBaseConfig.resolve, {
+      modules: baseConfig.resolve.modules,
+      extensions: baseConfig.resolve.extensions
+    }),
+    module: {
+      rules: [
+        ...storybookBaseConfig.module.rules,
+        ...baseConfig.module.rules.slice(2)
+      ]
+    }
+    // Object.assign({}, storybookBaseConfig.module, {
+    // rules: storybookBaseConfig.module.rules.concat(
+    //   baseConfig.module.rules.slice(1)
+  });
+
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve('ts-loader')
+      }
+    ]
+  });
+
+  return config;
+};
