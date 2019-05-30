adcfc1888b19fd5f120ab35d0bcb09d7d3bfb7e4
Paul Kim <paul.kim@catalystrms.com>
2019-05-30 13:09:30 -0600

    Replace webpack

--- private/storybook/webpack.config.js
+++ private/storybook/webpack.config.js
@@ -1,32 +1,49 @@
-/* eslint-disable arrow-parens */
+// /* eslint-disable arrow-parens */
 const baseConfig = require('../../webpack.config');
 
-module.exports = storybookBaseConfig => {
-  const config = Object.assign({}, storybookBaseConfig, {
-    resolve: Object.assign({}, storybookBaseConfig.resolve, {
-      modules: baseConfig.resolve.modules,
-      extensions: baseConfig.resolve.extensions,
-      alias: baseConfig.resolve.alias
-    }),
-    module: {
-      rules: [
-        ...storybookBaseConfig.module.rules,
-        ...baseConfig.module.rules.slice(2)
-      ]
-    }
-    // Object.assign({}, storybookBaseConfig.module, {
-    // rules: storybookBaseConfig.module.rules.concat(
-    //   baseConfig.module.rules.slice(1)
-  });
+// module.exports = storybookBaseConfig => {
+//   const config = Object.assign({}, storybookBaseConfig, {
+//     resolve: Object.assign({}, storybookBaseConfig.resolve, {
+//       modules: baseConfig.resolve.modules,
+//       extensions: baseConfig.resolve.extensions,
+//       alias: baseConfig.resolve.alias
+//     }),
+//     module: {
+//       rules: [
+//         ...storybookBaseConfig.module.rules,
+//         ...baseConfig.module.rules.slice(2)
+//       ]
+//     }
+//   });
 
+//   config.module.rules.push({
+//     test: /\.(ts|tsx)$/,
+//     use: [
+//       {
+//         loader: require.resolve('awesome-typescript-loader')
+//       }
+//     ]
+//   });
+
+//   return config;
+// };
+
+module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
       {
-        loader: require.resolve('ts-loader')
+        loader: require.resolve('awesome-typescript-loader')
+      },
+      {
+        loader: require.resolve('react-docgen-typescript-loader')
       }
     ]
   });
-
+  config.resolve.extensions.push('.ts', '.tsx');
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    ...baseConfig.resolve.alias
+  };
   return config;
 };
