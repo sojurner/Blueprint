04ad102e216c716c4d9dd876518b516e4d0b32b7
Paul Kim <paul.kim@catalystrms.com>
2019-05-30 13:09:17 -0600

    Modify config file

--- private/storybook/config.js
+++ private/storybook/config.js
@@ -1,20 +1,42 @@
-// https://github.com/diegohaz/arc/wiki/Storybook
-import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import theme from 'components/themes/default';
+import { configure, setAddon, addDecorator } from '@storybook/react';
+import JSXAddon from 'storybook-addon-jsx';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
 
-const req = require.context('components', true, /.stories.js$/);
+addDecorator(
+  withInfo({
+    styles: {
+      header: {
+        h1: {
+          marginRight: '20px',
+          fontSize: '25px',
+          display: 'inline'
+        },
+        body: {
+          paddingTop: 0,
+          paddingBottom: 0
+        },
+        h2: {
+          display: 'inline',
+          color: '#999'
+        }
+      },
+      infoBody: {
+        backgroundColor: 'almound',
+        padding: '0px 5px',
+        lineHeight: '2'
+      }
+    },
+    inline: false,
+    source: false
+  })
+);
+addDecorator(withKnobs);
+setAddon(JSXAddon);
+const req = require.context('../../src/storybook', true, /.stories.jsx$/);
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach(file => req(file));
 }
-
-addDecorator(story => (
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>{story()}</ThemeProvider>
-  </BrowserRouter>
-));
 
 configure(loadStories, module);
