bf1fb5dc72c4b44899f33fbb28ab836deeb8f15b
Paul Kim <paul.kim@catalystrms.com>
2019-05-31 08:07:29 -0600

    Add theme and style decorator to config

--- private/storybook/config.js
+++ private/storybook/config.js
@@ -1,7 +1,30 @@
+import React from 'react';
 import { configure, setAddon, addDecorator } from '@storybook/react';
 import JSXAddon from 'storybook-addon-jsx';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
+import ThemeWrapper from '@templates/material/ThemeWrapper';
+import { select } from '@storybook/addon-knobs/react';
+
+import { StylesProvider } from '@material-ui/styles';
+
+const colorSelection = {
+  orange: 'orange',
+  blue: 'blue',
+  green: 'green',
+  yellow: 'yellow',
+  pink: 'pink',
+  purple: 'purple',
+  indigo: 'indigo',
+  deepPurple: 'deepPurple',
+  lightBlue: 'lightBlue',
+  cyan: 'cyan',
+  teal: 'teal',
+  lightGreen: 'lightGreen',
+  lime: 'lime',
+  amber: 'amber',
+  deepOrange: 'deepOrange'
+};
 
 addDecorator(
   withInfo({
@@ -38,5 +61,11 @@ const req = require.context('../../src/storybook', true, /.stories.jsx$/);
 function loadStories() {
   req.keys().forEach(file => req(file));
 }
+
+addDecorator(story => (
+  <ThemeWrapper color={select('Color', colorSelection, 'orange', 'Theme')}>
+    <StylesProvider injectFirst>{story()}</StylesProvider>
+  </ThemeWrapper>
+));
 
 configure(loadStories, module);
