3bc7941ba025b1fc265197eadffcf78234fa67cd
Paul Kim <paul.kim@catalystrms.com>
2019-05-31 14:13:46 -0600

    Change file paths

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { IInputProps } from './types';
-import styleFactory from '../../../../factories/styleFactory';
+import styleFactory from '../../../factories/styleFactory';
 
 const LystInput: React.FunctionComponent<IInputProps> = ({
   styles,
